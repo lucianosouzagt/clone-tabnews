1e5bfd5037247b53b6e77f85e7b5295cbc667369
Luciano Souza <40190807+lucianosouzagt@users.noreply.github.com>
2024-10-07 12:55:06 +0000

    Adicionando o `prettier` como formatador de codigo padrão e scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do https://curso.dev, implementando o https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-function Home() { 
-    return  <>
-                <h1>Olá Mundo!</h1>
-                <h3>De todos os amores que eu tive és o mais antigo, o Vasco é minha vida, minha história, o meu primeiro amigo.</h3>
-            </>
+function Home() {
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <h3>
+        De todos os amores que eu tive és o mais antigo, o Vasco é minha vida,
+        minha história, o meu primeiro amigo.
+      </h3>
+    </>
+  );
 }
 
 export default Home;
