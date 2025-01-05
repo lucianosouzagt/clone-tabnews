ee2c63525f8f88f92121d756a17b367ecc92074b
Luciano Souza <40190807+lucianosouzagt@users.noreply.github.com>
2025-01-05 20:52:42 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/not-allowed-methods.test.js
+++ tests/integration/api/v1/migrations/not-allowed-methods.test.js
@@ -5,12 +5,16 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
 });
-test("Not allowed methods to api/v1/migrations should return 405", async () => {
-  const notAllowedMethods = ["PUT", "DELETE", "PATCH"];
-  for (const method of notAllowedMethods) {
-    let response = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: method,
+describe("PUT, DELETE, PATCH /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Checking not allowed methods", async () => {
+      const notAllowedMethods = ["PUT", "DELETE", "PATCH"];
+      for (const method of notAllowedMethods) {
+        let response = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: method,
+        });
+        expect(response.status).toBe(405);
+      }
     });
-    expect(response.status).toBe(405);
-  }
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,16 +3,19 @@ import orchestrator from "tests/orchestrator";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
+describe("POST /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+      expect(responseBody.update_at).toEqual(parsedUpdateAt);
 
-  const responseBody = await response.json();
-  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toEqual(parsedUpdateAt);
-
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
