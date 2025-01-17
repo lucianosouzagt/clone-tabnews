872863629b75cc5679dca7cbec14654a0bc45bda
Luciano Souza <40190807+lucianosouzagt@users.noreply.github.com>
2025-01-17 20:40:23 +0000

    feat: add initial version of /status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -22,15 +22,9 @@ function UpdateAt() {
     refreshInterval: 5000,
   });
   let updatedAtText = "Carregando...";
-  let db_version = "Carregando...";
-  let db_max_connections = "Carregando...";
-  let db_opened_connections = "Carregando...";
   if (!isLoading && data) {
     console.log(data);
     updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
-    db_version = data.dependencies.database.version;
-    db_max_connections = data.dependencies.database.max_connections;
-    db_opened_connections = data.dependencies.database.opened_connections;
   }
   return (
     <span style={{ fontSize: "15px" }}>
